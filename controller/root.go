da912252c8ab97b4f6fcde185c4f2905afce26ed
face0u0 <me@face0u0.net>
2021-02-24 23:02:55 +0900

    search pageを作成

--- controller/root.go
+++ controller/root.go
@@ -3,6 +3,7 @@ package controller
 import (
 	"net/http"
 
+	"github.com/gin-contrib/multitemplate"
 	"github.com/gin-gonic/gin"
 )
 
@@ -10,12 +11,23 @@ import (
 func RunServer() {
 
 	engine := gin.Default()
+	engine.LoadHTMLGlob("template/*.html")
+	renderer := multitemplate.NewRenderer()
+	engine.GET("/", index)
+	renderer.AddFromFiles("index", "template/layouts.html", "template/index.html")
 	engine.GET("/search", queryByTex)
+	renderer.AddFromFiles("search", "template/layouts.html", "template/search.html")
+	engine.HTMLRender = renderer
 	engine.Run(":3000")
 }
 
+func index(c *gin.Context) {
+	c.HTML(http.StatusOK, "index", gin.H{})
+}
+
 func queryByTex(c *gin.Context) {
-	c.JSON(http.StatusOK, gin.H{
-		"message": "hey!!",
+	queryStr := c.DefaultQuery("query", "")
+	c.HTML(http.StatusOK, "search", gin.H{
+		"query": queryStr,
 	})
 }
