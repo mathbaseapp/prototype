3c5c109820e769fc43cce8292a12ebc45aee233b
gyuta <kuroshiba0408@gmail.com>
2021-02-27 22:54:07 +0900

    コマンドライン引数でモードの切り替えを行う

--- main.go
+++ main.go
@@ -1,9 +1,25 @@
 package main
 
 // import "prototype.mathbase.app/crawler"
-import "prototype.mathbase.app/controller"
+import (
+	"flag"
+
+	"prototype.mathbase.app/crawler"
+
+	"prototype.mathbase.app/controller"
+)
 
 func main() {
-	// crawler.Crawle()
-	controller.RunServer()
+	// usage: go run main.go -mode=crawl
+	mode := flag.String("mode", "server", "represents mode which program will be started in. server or crawl is available. default is server.")
+	flag.Parse()
+
+	switch *mode {
+	case "server":
+		controller.RunServer()
+	case "crawl":
+		crawler.Crawle()
+	default:
+		panic("the argument of mode is invalid. only server or crawl are allowed.")
+	}
 }
