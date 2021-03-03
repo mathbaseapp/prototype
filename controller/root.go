1975022a4b0bef71f1589a84768293cbd2fc7163
face0u0 <hiroki@e.face0u0.net>
2021-03-03 18:12:06 +0900

    add cors package

--- controller/root.go
+++ controller/root.go
@@ -3,6 +3,7 @@ package controller
 import (
 	"net/http"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-contrib/multitemplate"
 	"github.com/gin-gonic/gin"
 	"prototype.mathbase.app/service"
@@ -12,6 +13,12 @@ import (
 func RunServer() {
 
 	engine := gin.Default()
+
+	// cors対応
+	config := cors.DefaultConfig()
+	config.AllowOrigins = []string{"https://mathbase.app"}
+	engine.Use(cors.New(config))
+
 	engine.LoadHTMLGlob("template/*.html")
 	renderer := multitemplate.NewRenderer()
 	engine.GET("/", index)
@@ -19,6 +26,8 @@ func RunServer() {
 	engine.GET("/search", queryByTex)
 	renderer.AddFromFiles("search", "template/layouts.html", "template/search.html")
 	engine.HTMLRender = renderer
+	engine.GET("/api/v1/search", restQueryByTex)
+
 	engine.Run(":3000")
 }
 
@@ -35,5 +44,19 @@ func queryByTex(c *gin.Context) {
 	c.HTML(http.StatusOK, "search", gin.H{
 		"query":   queryStr,
 		"results": results,
+	})
+}
+
+func restQueryByTex(c *gin.Context) {
+	queryStr := c.DefaultQuery("query", "")
+	results, err := service.QueryByLatex(queryStr)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": err.Error(),
+		})
+	}
+	c.JSON(http.StatusOK, gin.H{
+		"results": results,
+		"query":   queryStr,
 	})
 }
--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module prototype.mathbase.app
 go 1.13
 
 require (
+	github.com/gin-contrib/cors v1.3.1
 	github.com/gin-contrib/multitemplate v0.0.0-20200916052041-666a7309d230
 	github.com/gin-gonic/gin v1.6.3
 	github.com/google/uuid v1.2.0
